d78db5b5a31a5de3eb19c1bacb3bf89aaa543a55
Soccer Liu <Cheng-mean Liu>
2018-01-28 15:10:21 -0800

    cleaned up

--- PortForwardingNat_old/proxy/startup.bat
+++
@@ -1,7 +0,0 @@
-echo "Start running setupproxynet.ps1"
-
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\setupproxynet.ps1'"
-
-echo "Launch a webserver for listing to client container request"
-
-python .\app.py
